2ddb78fc0ddfbdbc01e8cddbcade84dca2f148ab
Viktor <kozzlenko@yandex.ru>
2019-05-03 23:25:41 +0700

    Starting security

--- src/test/java/com/taskmanager/web/apis/RegistrationApiControllerTests.java
+++ src/test/java/com/taskmanager/web/apis/RegistrationApiControllerTests.java
@@ -1,6 +1,8 @@
 package com.taskmanager.web.apis;
 
+import com.taskmanager.config.SecurityConfiguration;
 import com.taskmanager.domain.application.UserService;
+import com.taskmanager.domain.model.user.EmailAddressExistsException;
 import com.taskmanager.domain.model.user.UsernameExistsException;
 import com.taskmanager.utils.JsonUtils;
 import com.taskmanager.web.payload.RegistrationPayload;
@@ -10,17 +12,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {SecurityConfiguration.class, RegistrationApiController.class})
 @WebMvcTest(RegistrationApiController.class)
+@ActiveProfiles("test")
 public class RegistrationApiControllerTests {
+
     @Autowired
     private MockMvc mvc;
 
@@ -28,8 +36,7 @@ public class RegistrationApiControllerTests {
     private UserService serviceMock;
 
     @Test
-    public void register_blankPayload_shouldFailAndReturn400() throws
-            Exception {
+    public void register_blankPayload_shouldFailAndReturn400() throws Exception {
         mvc.perform(post("/api/registrations"))
                 .andExpect(status().is(400));
     }
@@ -40,14 +47,52 @@ public class RegistrationApiControllerTests {
         payload.setUsername("exist");
         payload.setEmailAddress("test@taskagile.com");
         payload.setPassword("MyPassword!");
+
         doThrow(UsernameExistsException.class)
                 .when(serviceMock)
                 .register(payload.toCommand());
+
         mvc.perform(
                 post("/api/registrations")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JsonUtils.toJson(payload)))
                 .andExpect(status().is(400))
                 .andExpect(jsonPath("$.message").value("Username already exists"));
+    }
+
+    @Test
+    public void register_existedEmailAddress_shouldFailAndReturn400() throws Exception {
+        RegistrationPayload payload = new RegistrationPayload();
+        payload.setUsername("test");
+        payload.setEmailAddress("exist@taskagile.com");
+        payload.setPassword("MyPassword!");
+
+        doThrow(EmailAddressExistsException.class)
+                .when(serviceMock)
+                .register(payload.toCommand());
+
+        mvc.perform(
+                post("/api/registrations")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.toJson(payload)))
+                .andExpect(status().is(400))
+                .andExpect(jsonPath("$.message").value("Email address already exists"));
+    }
+
+    @Test
+    public void register_validPayload_shouldSucceedAndReturn201() throws Exception {
+        RegistrationPayload payload = new RegistrationPayload();
+        payload.setUsername("sunny");
+        payload.setEmailAddress("sunny@taskagile.com");
+        payload.setPassword("MyPassword!");
+
+        doNothing().when(serviceMock)
+                .register(payload.toCommand());
+
+        mvc.perform(
+                post("/api/registrations")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.toJson(payload)))
+                .andExpect(status().is(201));
     }
 }

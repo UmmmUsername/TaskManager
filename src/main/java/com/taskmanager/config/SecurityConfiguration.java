95a4ddf4e32b6783cfaa8fc8092f85296dec3cde
Viktor <kozzlenko@yandex.ru>
2019-05-05 22:31:23 +0700

    Authorization impl

--- src/main/java/com/taskmanager/config/SecurityConfiguration.java
+++ src/main/java/com/taskmanager/config/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package com.taskmanager.config;
 
+import com.taskmanager.web.apis.authenticate.AuthenticationFilter;
+import com.taskmanager.web.apis.authenticate.SimpleAuthenticationFailureHandler;
+import com.taskmanager.web.apis.authenticate.SimpleAuthenticationSuccessHandler;
+import com.taskmanager.web.apis.authenticate.SimpleLogoutSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -7,6 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,24 +29,48 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC).permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterAt(authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .formLogin()
                 .loginPage("/login")
                 .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logged-out")
+                .logoutSuccessHandler(logoutSuccessHandler())
                 .and()
                 .csrf().disable();
     }
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/static/**", "/js/**", "/css/**",
-                "/images/**", "/favicon.ico");
+        web.ignoring().antMatchers("/static/**", "/js/**", "/css/**", "/images/**", "/favicon.ico");
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationFilter authenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
+        authenticationFilter.setAuthenticationFailureHandler(authenticationFailureHandler());
+        authenticationFilter.setAuthenticationManager(authenticationManagerBean());
+        return authenticationFilter;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new SimpleAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new SimpleAuthenticationFailureHandler();
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return new SimpleLogoutSuccessHandler();
     }
 }
--- src/main/java/com/taskmanager/domain/application/UserService.java
+++ src/main/java/com/taskmanager/domain/application/UserService.java
@@ -2,7 +2,17 @@ package com.taskmanager.domain.application;
 
 import com.taskmanager.domain.application.commands.RegistrationCommand;
 import com.taskmanager.domain.model.user.RegistrationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
+
+    /**
+     * Register a new user with username, email address, and password.
+     *
+     * @param command instance of <code>RegistrationCommand</code>
+     * @throws RegistrationException when registration failed. Possible reasons are:
+     *                               1) Username already exists
+     *                               2) Email address already exists.
+     */
     void register(RegistrationCommand command) throws RegistrationException;
 }
